36d3980ead763d0c95505e432ffc13e78f7f2bdd
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-07-11 23:19:27 +0000

    show snake

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js";
 
 let lastRenderTime = 0
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
+    window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
@@ -22,6 +24,6 @@ function update() {
 }
 
 function draw() {
-    drawSnake()
+    drawSnake(gameBoard)
 
 }
