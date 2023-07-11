ad5d933ddf3e29968fb12ae09050e146d09a030f
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-07-11 23:01:08 +0000

    update file

--- game.js
+++ game.js
@@ -1,10 +1,27 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js";
+
 let lastRenderTime = 0
 
 function main(currentTime) {
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    window.requestAnimationFrame(main)
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
     lastRenderTime = currentTime
-    console.log(currentTime); 
+
+    update()
+    draw()
+     
 }
 
 window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+
+}
+
+function draw() {
+    drawSnake()
+
+}
